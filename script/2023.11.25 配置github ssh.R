377e7a3e0e3a5725c234aaa333822bb848ca574b
Hong-Chaoli <hongchl3@outlook.com>
2023-11-25 19:26:22 +0800

    配置github ssh

---
+++ script/2023.11.25 配置github ssh.R
@@ -0,0 +1,9 @@
+# 2023.11.25 配置和添加新的ssh公钥。公钥保存在 id_rsa.pub 文件中。以下为Terminal运行内容：
+# Last login: Sat Nov 25 19:14:57 on ttys000
+# hongchaoli@Hong-Chaoli-MBP ~ % cd ~/.ssh
+# hongchaoli@Hong-Chaoli-MBP .ssh % ls
+# config		id_ed25519.pub	id_rsa.pub	known_hosts.old
+# id_ed25519	id_rsa		known_hosts
+# hongchaoli@Hong-Chaoli-MBP .ssh % cat id_rsa.pub
+# ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDV6CYL64h9Dhh3Lbb5vvwtYewAfKHDRLrNDEDwhVw+cjr9Ulus1jbW3VJVzS7HCvnaqcScpG4Ww7aLClNScjj4tdyQXoXAY2hJbmf1BvrNg6nBwjJ7pWDcW9PdXUTPqLHK9c/jMSkes6Bmxey+ZknMkOJX4q27H20xmYjqvPByBV6XOydRbfR8N8es5cYXmG3SXUdppo6celcTzrzsmtVpHOxzcTL72rNCxSOBRdfzueCgcjLkGTu0NRZo/jKMMs/IbHNH1eKLnHnb3QHs9F+1zPnABQE7Kp1rDtUqSxouyaYg9mOehJuf0jzm+rjJZ5M0ZoQDsFpnIzaou2HW5NUqDDih2kPL5q5RhjPJvx92F5r5VevqaRqoLltzNUNy4H1wyHAX2/0kZ6uRWNfDqNwfqvs9wvUFx2iB+TGtiUgUOodyqksnpHmt3H22NsPPa533eU0TCARTOdSuakniSrrCiK3QycAJ3mFd82qGEgpSjPtTlMlqu+tNvQZwExIOVQ/vnrXFtscFwxTVJ0gRKW0JRUdD0qWA7rZIVAevR9NCY2hWwRp/qco4AcXDxbtJUi5Vieu4qeRvrZKERaPkqZsFXsMBx5orXUzwhcXRpSoZOd/ai8C8h5t7fiMCY726QeWOR4aweax48NfzmWSyC1+nk0jtbqnihCjyWAB9yY9pSQ== hongchl3@outlook.com
+# hongchaoli@Hong-Chaoli-MBP .ssh % 
