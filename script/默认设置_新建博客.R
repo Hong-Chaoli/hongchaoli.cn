0bf6a475b981782eae02e7c32043956b59c258eb
Hong-Chaoli <hongchl3@outlook.com>
2023-11-25 18:50:38 +0800

    搭建网站

---
+++ script/默认设置_新建博客.R
@@ -0,0 +1,45 @@
+# 主题：yihui/hugo-lithium
+# blogdown's introduction:
+# https://bookdown.org/yihui/blogdown/a-quick-example.html
+
+# 用 R 语言的 blogdown + hugo + netlify + github 搭建静态博客系统
+# https://cosx.org/2018/01/build-blog-with-blogdown-hugo-netlify-github/
+# rstudio连接到github -》github连接到netlify进行deployment -》将自己注册的域名绑定到netlify
+# -》在个人域名的提供商网站修改nameservers为netlify提供的nameservers。
+
+# 更改icon:
+# config.yaml中找到favicon: favicon.ico
+# theme -> hugo-lithium -> static -> favicon.ico
+# theme -> hugo-lithium -> static -> images -> logo.png
+
+library(blogdown)
+check_gitignore()
+check_content()
+hugo_version()
+serve_site()
+
+#新建博客 .Rmd文件
+blogdown::new_post()
+blogdown::new_post(title = "2022-12-02 Hubel Wiesel 1959 猫纹状皮层神经元的receptive field（单单元记录）",  
+                   ext = '.Rmd', 
+                   subdir = "post")
+
+#启动本地服务，一边写文档，网页渲染效果随时可见
+blogdown::serve_site()
+
+#停止本地服务
+blogdown::stop_server()
+
+#将.Rmd文件渲染为html等站点文件
+blogdown::build_site()
+
+
+
+
+
+
+
+
+
+
+
